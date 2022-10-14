9a8b06bf54235c2d80b4b6e3c1e0895bc6048ef2
Marc Luqué <marc.luque@outlook.com>
2022-10-14 23:31:10 +0200

    chore: update include guard names to be standard: PROJECTNAME_MODULE_H

--- src/mmu.h
+++ src/mmu.h
@@ -1,5 +1,6 @@
-#ifndef MEM_H
-#define MEM_H
+#ifndef YOBEMAG_MEM_H
+#define YOBEMAG_MEM_H
+
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,4 +14,4 @@ void mmu_write_byte(uint16_t dest_addr, uint8_t source_addr);
 __attribute__((pure)) uint16_t mmu_get_two_bytes(uint16_t addr);
 void mmu_write_two_bytes(uint16_t dest_addr, uint16_t value);
 
-#endif
+#endif //YOBEMAG_MEM_H
