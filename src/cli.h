3dca0ae8e3bc83f79a33ba16ff91edeeb583a875
Moritz Reinhard Hüllmann <moritz.huellmann@rwth-aachen.de>
2022-10-16 23:57:05 +0200

    chore: add cli and logging doxygen comments

--- src/cli.h
+++ src/cli.h
@@ -11,14 +11,25 @@ typedef struct CLIArguments {
     const char* rom_path;
 } CLIArguments;
 
+/**
+ * @brief   Allocates a struct to store CLI arguments in and returns a pointer to that struct
+ *
+ * @warning The allocated memory must be freed by using @p cli_config_destroy 
+ * @returns Pointer to a CLIArguments struct
+ */
 CLIArguments* cli_config_default(void);
 
+/**
+ * @brief   Deallocates the memory, where @p conf points to
+ *
+ * @param   conf    Pointer to @p CLIArguments
+ */
 void cli_config_destroy(CLIArguments *const conf);
 
 /**
  * @brief   Extract CLI flags with (optional) arguments from @p argv
  *
- * @param   conf    Pointer to @CLIArguments
+ * @param   conf    Pointer to @p CLIArguments
  * @param   argc    Number of arguments passed to the program
  * @param   argv    The passed arguments
  *
--- src/logging.h
+++ src/logging.h
@@ -11,8 +11,22 @@ typedef enum LoggingLevel {
     FATAL
 } LoggingLevel;
 
-void log_str(const LoggingLevel dbg_lvl, const char* const dbg_lvl_str, FILE *const stream, const char* const msg, ...);
+/**
+ * @brief   Log a string to @p stream with logging level @p dbg_lvl, (printed as @p log_lvl_str), formatted as @p msg with parameters @p ...
+ *
+ * @param   log_lvl     Logging level, represented as enum
+ * @param   log_lvl_str Logging level string representation
+ * @param   stream      Stream to print to (commonly stderr/stdout)
+ * @param   msg         Format string for the log message
+ * @param   ...         Parameters for format string
+ */
+void log_str(const LoggingLevel log_lvl, const char* const log_lvl_str, FILE *const stream, const char* const msg, ...);
 
+/**
+ * @brief   Set the minimum required of a log message to be printed to the console
+ *
+ * @param   log_lvl The desired minimal logging level
+ */
 void log_set_lvl(LoggingLevel log_lvl);
 
 #define LOG_DEBUG(msg, ...)     log_str(DEBUG, "DEBUG", stdout, msg, ##__VA_ARGS__)
