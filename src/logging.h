1eb4e10e8d656928784a581d7c54f072c1de3df4
Moritz Reinhard Hüllmann <moritz.huellmann@rwth-aachen.de>
2022-10-16 23:58:35 +0200

    chore: remove warning regarding variadic functions for logging

--- src/logging.h
+++ src/logging.h
@@ -29,10 +29,15 @@ void log_str(const LoggingLevel log_lvl, const char* const log_lvl_str, FILE *co
  */
 void log_set_lvl(LoggingLevel log_lvl);
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wgnu-zero-variadic-macro-arguments"
+
 #define LOG_DEBUG(msg, ...)     log_str(DEBUG, "DEBUG", stdout, msg, ##__VA_ARGS__)
 #define LOG_INFO(msg, ...)      log_str(INFO, "INFO", stdout, msg, ##__VA_ARGS__)
 #define LOG_WARNING(msg, ...)   log_str(WARNING, "WARNING", stdout, msg, ##__VA_ARGS__)
 #define LOG_ERROR(msg, ...)     log_str(ERROR, "ERROR", stderr, msg, ##__VA_ARGS__)
 #define LOG_FATAL(msg, ...)     log_str(FATAL, "FATAL", stderr, msg, ##__VA_ARGS__)
+
+#pragma clang diagnostic pop 
 
 #endif //YOBEMAG_LOGGING_H
