d371269db2ebd7c109c10aa906b0b6b1a1689882
Samir Benzammour <samir.benzammour@gmail.com>
2022-10-18 19:35:24 +0200

    chore: convert tabs to spaces

--- test/alu_8bit_sub_test.c
+++ test/alu_8bit_sub_test.c
@@ -1,52 +1,51 @@
 #include <criterion/criterion.h>
 #include <stdio.h>
 #include <time.h>
+
 #include "cli.h"
 #include "cpu.h"
 #include "logging.h"
 #include "mmu.h"
 
 void cpu_setup(void) {
-	srand(time(NULL));
+    srand(time(NULL));
     mmu_init();
-	cpu_init();
+    cpu_init();
 }
 
-void cpu_teardown(void) {
-	mmu_destroy();
-}
+void cpu_teardown(void) { mmu_destroy(); }
 
 // Test SUB_A_A with no borrow
 Test(simple, sub_a_a_no_borrow, .init = cpu_setup, .fini = cpu_teardown) {
-	const uint8_t opcode = 0x97;
-	const uint8_t a = 128;
-	const uint8_t expected = 0;
-	const uint16_t address = rand() % (0xFFFF);
+    const uint8_t opcode = 0x97;
+    const uint8_t a = 128;
+    const uint8_t expected = 0;
+    const uint16_t address = rand() % (0xFFFF);
 
-	// setup cpu
-	cpu.PC = address;
-	cpu.AF.bytes.high = a;
-	mmu_write_byte(address, opcode);
+    // setup cpu
+    cpu.PC = address;
+    cpu.AF.bytes.high = a;
+    mmu_write_byte(address, opcode);
 
-	// do the actual emulation
-	cpu_step();
-	const uint8_t actual = cpu.AF.bytes.high;
-	
-	// Z should be set, as our result is 0
-	cr_assert(cpu.AF.bytes.low & (1 << Z_FLAG));
+    // do the actual emulation
+    cpu_step();
+    const uint8_t actual = cpu.AF.bytes.high;
 
-	// N should be set
-	cr_assert(cpu.AF.bytes.low & (1 << N_FLAG));
+    // Z should be set, as our result is 0
+    cr_assert(cpu.AF.bytes.low & (1 << Z_FLAG));
 
-	// H should NOT be set
-	cr_assert(!(cpu.AF.bytes.low & (1 << H_FLAG)));
+    // N should be set
+    cr_assert(cpu.AF.bytes.low & (1 << N_FLAG));
 
-	// C should NOT be set
-	cr_assert(!(cpu.AF.bytes.low & (1 << C_FLAG)));
-	
-	// check if value is correct
-	cr_assert(actual == expected);
+    // H should NOT be set
+    cr_assert(!(cpu.AF.bytes.low & (1 << H_FLAG)));
 
-	// check if pc is Pc is updated correctly
-	cr_assert(cpu.PC == (address + 1));
+    // C should NOT be set
+    cr_assert(!(cpu.AF.bytes.low & (1 << C_FLAG)));
+
+    // check if value is correct
+    cr_assert(actual == expected);
+
+    // check if pc is Pc is updated correctly
+    cr_assert(cpu.PC == (address + 1));
 }
--- test/sample_unit_test.c
+++ test/sample_unit_test.c
@@ -3,17 +3,17 @@
 #include "cli.h"
 
 void setup(void) {
-	puts("Runs before the test");
+    puts("Runs before the test");
 }
 
 void teardown(void) {
-	puts("Runs after the test");
+    puts("Runs after the test");
 }
 
 Test(simple, fixtures, .init = setup, .fini = teardown) {
-	CLIArguments* c = cli_config_default();
-	cr_assert(c != NULL);
-	cli_config_destroy(c);
-	c = NULL;
-	cr_assert(c == NULL);
+    CLIArguments* c = cli_config_default();
+    cr_assert(c != NULL);
+    cli_config_destroy(c);
+    c = NULL;
+    cr_assert(c == NULL);
 }
